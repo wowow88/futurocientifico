d137c3fa903301e2b30303246cf8a35d659776fb
wowow88 <j_m_m_verdejo@hotmail.com>
2025-06-01 22:22:19 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 
+// https://docs.astro.build/en/reference/configuration-reference/
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), mdx(), content()],
-  site: 'https://futurocientifico.vercel.app'
+  site: 'https://futurocientifico.vercel.app',
+  integrations: [tailwind(), sitemap(), mdx()]
 });
