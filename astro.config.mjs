e0166b8cdd0368c4881eeb4bed3d2f148e4bee2e
wowow88 <j_m_m_verdejo@hotmail.com>
2025-06-01 22:14:32 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
-import content from '@astrojs/content';
 
 export default defineConfig({
   integrations: [tailwind(), sitemap(), mdx(), content()],
