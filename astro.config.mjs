86c1370a641edb5ae3b2848f2b46f9ba08e7461b
wowow88 <j_m_m_verdejo@hotmail.com>
2026-01-14 12:19:16 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
-import vercelStatic from '@astrojs/vercel-static';
+import vercel from '@astrojs/vercel/static';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://futurocientifico.es',
-  trailingSlash: 'never',
   output: 'static',
-  adapter: vercelStatic(),
+  trailingSlash: 'never',
+  adapter: vercel({ split: false }),
   integrations: [tailwind(), mdx()],
 });
+
 
